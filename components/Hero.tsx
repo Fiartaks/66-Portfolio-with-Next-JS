ffc066790b68b6fd2e93e3f321d959c10c5cc846
fiartaks <fiartaks@gmail.com>
2025-02-27 00:53:04 +0300

    index.ts added some image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,13 +26,14 @@ const Hero = () => {
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 ">
             Dynamic Web Magic with Next.js
+
           </h2>
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Hi, I&apos;m Fırat, a Full Stack Developer based in Türkiye. "
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-           Hi, I&apos;m Fırat, a Next.js Developer based in Türkiye.
+          Transforming Design into Seamless User Experiences 
           </p>
           <a href="#about">
             <MagicButton
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -18,7 +18,7 @@ const RecentProjects = () => {
           key={`${item.id}-${index}`} // Benzersiz key için id ve indeksi birleştirdik
           >
             <PinContainer
-              title="/ui.aceternity.com"
+              title="fiartaks"
               href="https://twitter.com/mannupaaji"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
