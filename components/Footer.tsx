13d2600bf02e3b52067dafcaf612f37ba75546a2
fiartaks <fiartaks@gmail.com>
2025-03-04 23:45:20 +0300

    a href added with links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -45,12 +45,15 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Image
-                src={info.img}
-                alt="icons"
-                width={20} // genişlik
-                height={20} // yükseklik
-              />
+              <a href={info.link} target="_blank" rel="noopener noreferrer">
+                <Image
+                  src={info.img}
+                  alt="icons"
+                  width={20}
+                  height={20}
+                  style={{ cursor: "pointer" }} // Kullanıcıya tıklanabilir olduğunu göstermek için
+                />
+              </a>
             </div>
           ))}
         </div>
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -78,9 +78,14 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                <a
+                    href={item.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex lg:text-xl md:text-xs text-sm text-purple"
+                  >
                     Check Live Site
-                  </p>
+                  </a>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
